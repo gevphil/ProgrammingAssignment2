f009c80d5f5a194e6f8e5f730db41028f264cb32
Grant Phillips <gevphil@gmail.com>
2019-01-14 21:09:20 -0600

    Adding cachematrix.R for programmingAssigmnet2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matrix <- x
+    matrixInverse <- NULL
+    prevMat <- NULL
+    set <- function(x) {
+              matrix <<- x
+    }
+    get <- function() matrix
+    setPrevMat <- function(z) {
+              prevMat <<- z
+    }
+    getPrevMat <- function() prevMat
+    setInverse <- function(y) {
+              matrixInverse <<- y
+    }
+    getInverse <- function() matrixInverse
+    
+    list(set = set, get = get, setPrevMat = setPrevMat, getPrevMat = getPrevMat, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    matrix <- x$get()
+    matrixInverse <- x$getInverse()
+    PrevMat <- x$getPrevMat()
+    if(is.null(matrixInverse) == FALSE & all.equal(PrevMat,matrix) == TRUE) {
+        message("getting cached inverse matrix")
+        return(matrixInverse)
+    }
+    x$setPrevMat(matrix)
+    matrixInverse <- solve(matrix)
+    x$setInverse(matrixInverse)
+    matrixInverse
 }
